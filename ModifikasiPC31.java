01d16aad715532207c0f42346d9403b842e3dd9e
ayaa <aaurellia23@gmail.com>
2024-10-29 21:15:17 +0700

    ModifikasiPertama

--- ModifikasiPC31.java
+++ ModifikasiPC31.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
-public class ArrayRataNilai03 {
+public class ModifikasiPC31 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] nilaiMahasiswa = new int[10];
         double total = 0, rataNilai;
+        int siswaLulus = 0;
+        
         for (int i = 0; i < nilaiMahasiswa.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + " : ");
             nilaiMahasiswa[i] = input.nextInt();
         }
         for (int i = 0; i < nilaiMahasiswa.length; i++) {
             total += nilaiMahasiswa[i];
+            if (nilaiMahasiswa[i] > 70) {
+                siswaLulus += 1;
+            }
         }
         rataNilai = total / nilaiMahasiswa.length;
         System.out.println("Rata-Rata nilai Mahasiswa : " + rataNilai);
+        System.out.println("Mahasiswa dengan nilai lebih dari 70 sebanyak: " + siswaLulus);
+
         input.close();
     }
 }
