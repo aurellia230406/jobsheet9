6fbdb4f49088065d6d82aeeb5b97ddcf7b58122b
ayaa <aaurellia23@gmail.com>
2024-10-29 21:05:31 +0700

    ModifikasiPC2

--- ArrayNilai03.java
+++ ArrayNilai03.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class ArrayNilai03 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
         for (int i = 0; i < 10; i++) {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
@@ -9,5 +10,6 @@ public class ArrayNilai03 {
         for (int i = 0; i < 10; i++) {
             System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
         }
+        sc.close();
     }
 }
